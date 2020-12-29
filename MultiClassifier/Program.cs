fab3a071340e267d973e36a680b3f59d694afb0f
Colin Bird <colin.bird.86@gmail.com>
2020-12-29 11:27:31 -0500

    Initial Commit

---
+++ MultiClassifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiClassifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
